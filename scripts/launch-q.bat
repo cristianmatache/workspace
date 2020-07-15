1cbc1a29e639bf6cf74d9a70bad815a71136318a
Cristian Matache <cristianmatache@hotmail.com>
2020-07-15 21:24:46 +0100

    launch q.bat

---
+++ scripts/launch-q.bat
@@ -0,0 +1,7 @@
+SET QHOME=C:\q
+SET QLIC=C:\q
+SET DEVELOPER_HOME=D:\workspace\q\developer
+SET DEVELOPER_DATA=D:\workspace\q\developer\data
+SET AXLIBRARIES_HOME=D:\workspace\q\developer
+SET PATH=%PATH%%AXLIBRARIES_HOME%\ws\lib
+C:\q\w64\q.exe -p 5000 -U C:\q\credentials.txt

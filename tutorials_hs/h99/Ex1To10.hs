0b3ad7114dc2bbbc83218e13b6c6e6c361a5c34b
Cristian Matache <cristianmatache@hotmail.com>
2020-12-31 16:56:38 +0000

    hlint

--- tutorials_hs/h99/Ex1To10.hs
+++ tutorials_hs/h99/Ex1To10.hs
@@ -2,22 +2,22 @@ module Ex1To10 where
 
 -- Ex 1
 myLast :: [a] -> a
-myLast (x:[]) = x
+myLast [x] = x
 myLast (_:xs) = myLast xs
 
 safeMyLast :: [a] -> Maybe a
 safeMyLast [] = Nothing
-safeMyLast (x:[]) = Just x
+safeMyLast [x] = Just x
 safeMyLast (_:xs) = safeMyLast xs
 
 
 -- Ex2
 myButLast :: [a] -> a
-myButLast (x:_:[]) = x
+myButLast [x, _] = x
 myButLast (_:xs) = myButLast xs
 
 safeMyButLast :: [a] -> Maybe a
 safeMyButLast [] = Nothing
-safeMyButLast (_:[]) = Nothing
-safeMyButLast (x:_:[]) = Just x
+safeMyButLast [_] = Nothing
+safeMyButLast [x, _] = Just x
 safeMyButLast (_:xs) = safeMyButLast xs

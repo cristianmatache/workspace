81708b832fbf1b962cce7cf3d5b0db77005fd17a
Cristian Matache <cristianmatache@hotmail.com>
2021-05-29 12:32:09 +0100

    start airflow

--- 3rdparty/airflow/manage/start_airflow.sh
+++ 3rdparty/airflow/manage/start_airflow.sh
@@ -2,6 +2,10 @@
 
 ENV='/home/cristian/apps/miniconda3/envs/airflow/bin'
 export PATH="$ENV":"$PATH"
+echo "Using environment: $ENV"
+
+AIRFLOW_BASE="${AIRFLOW_HOME:-~/airflow}"
+rm -rf "$AIRFLOW_BASE/airflow-*"
 
 airflow webserver --port 7000 -D
 airflow scheduler -D

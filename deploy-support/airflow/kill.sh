5e39f01eed205223a33d8339a84961b04d24d38d
Cristian Matache <cristianmatache@hotmail.com>
2021-06-09 22:47:28 +0100

    kill airflow pids

--- deploy-support/airflow/kill.sh
+++ deploy-support/airflow/kill.sh
@@ -4,4 +4,7 @@
 pkill -f "airflow webserver" && echo "Airflow webserver was killed"
 pkill -f "airflow scheduler" && echo "Airflow scheduler was killed"
 
+AIRFLOW_HOME="${AIRFLOW_HOME:-$HOME/airflow}"
+rm -rf "$AIRFLOW_HOME/*.pid"
+
 sleep 2s

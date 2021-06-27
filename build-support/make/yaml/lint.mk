59207816c3e254680e6f3fe64c5bf63094dd41f2
Cristian Matache <cristianmatache@hotmail.com>
2021-06-27 19:38:42 +0100

    strict yamllint

--- build-support/make/yaml/lint.mk
+++ build-support/make/yaml/lint.mk
@@ -4,8 +4,8 @@ yamllint:
 	$(eval on := $(onyml))
 ifeq ($(since),)
 	if $(call lang,$(on),".*\.y[a]ml"); then \
-	python -m yamllint -c $(YAMLLINT_CONFIG) $(call solve_on,$(on)); fi
+	python -m yamllint --strict -c $(YAMLLINT_CONFIG) $(call solve_on,$(on)); fi
 else
 	if $(call lang,$(call solve_since,$(since),".yml"),".*\.ya?ml"); then \
-	python -m yamllint -c $(YAMLLINT_CONFIG) $(call solve_since,$(since),".yml"); fi
+	python -m yamllint --strict -c $(YAMLLINT_CONFIG) $(call solve_since,$(since),".yml"); fi
 endif

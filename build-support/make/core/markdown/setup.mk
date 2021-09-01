4057d11c60e23b412e41eeb6a81508e407a2f872
Cristian Matache <cristianmatache@hotmail.com>
2021-09-01 21:55:21 +0100

    add registries

--- build-support/make/core/markdown/setup.mk
+++ build-support/make/core/markdown/setup.mk
@@ -1,5 +1,5 @@
 env-md-default-upgrade:
-	cat $(DEFAULT_NPM_DEV_MD_DEPS) | tr -d "\r" | xargs npm --prefix $(DEFAULT_MD_ENV) install --save-dev
+	cat $(DEFAULT_NPM_DEV_MD_DEPS) | tr -d "\r" | xargs npm --prefix $(DEFAULT_MD_ENV) install --save-dev  --registry=https://registry.npmjs.org
 
 env-md-default-replicate:
-	npm --prefix $(DEFAULT_MD_ENV) ci || echo "Check manually if it passes, since it causes make to fail unexpectedly !!!"
+	npm --prefix $(DEFAULT_MD_ENV) ci --registry=https://registry.npmjs.org || echo "Check manually if it passes, since it causes make to fail unexpectedly !!!"

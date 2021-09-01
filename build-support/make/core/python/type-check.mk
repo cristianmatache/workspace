80e42406ad02c8c8a12ae48d43fca89aa2e4283b
Cristian Matache <cristianmatache@hotmail.com>
2021-09-01 16:40:01 +0100

    Update type-check.mk

--- build-support/make/core/python/type-check.mk
+++ build-support/make/core/python/type-check.mk
@@ -1,4 +1,6 @@
 mypy:
 	$(eval on := $(onpy))
 	if $(call lang,$(on),".*\.pyi?"); then  \
+	pip freeze
+	pip show types-requests
 	python -m mypy --config-file $(MYPY_CONFIG) $(call solve_on,$(on)); fi

a4bebf544e44f3b0728a26d470a55ba611e63884
Cristian Matache <cristianmatache@hotmail.com>
2021-09-01 16:52:30 +0100

    py3.9 mypypath

--- build-support/make/core/bash/lint.mk
+++ build-support/make/core/bash/lint.mk
@@ -5,5 +5,6 @@ shellcheck:
 	find $(call solve_on,$(on)) -type f -iname "*.sh" | xargs shellcheck -x --format=gcc -e SC1017; fi
 
 shfmt-check:
+	$(eval on := $(onsh))
 	if $(call lang,$(on),".*\.sh"); then \
 	shfmt -d $(call solve_on,$(on)); fi

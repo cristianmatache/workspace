3222a829a99ff1cd6ba320d2d5cb256949072b1e
Cristian Matache <cristianmatache@hotmail.com>
2021-09-11 17:03:49 +0100

    prometheus and alertmanager extensions

--- build-support/make/extensions/alertmanager/lint.mk
+++ build-support/make/extensions/alertmanager/lint.mk
@@ -1,6 +1,6 @@
 # find .. --exec always has exit code 0 -> use find | xargs
 amtool-check-config:
-	$(eval targets := $(onam))
+	$(eval on := $(onam))
 	$(eval amhome := $(shell source lib_sh_utils/src/commands.sh && find_command_home alertmanager ~/alertmanager/ ALERTMANAGER_HOME))
 	if $(call lang,$(targets),".*alertmanager/.*\.yml"); then \
-	find $(targets) -type f -regex ".*alertmanager.*\.yml" | xargs --no-run-if-empty "$(amhome)/amtool" check-config; fi
+	find $(call solve_aliases,$(on)) -type f -regex ".*alertmanager.*\.yml" | xargs --no-run-if-empty "$(amhome)/amtool" check-config; fi

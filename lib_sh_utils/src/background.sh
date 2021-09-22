9b9ccc84123d99918d29351648acb5946cf6f396
Cristian Matache <cristianmatache@hotmail.com>
2021-09-22 22:35:38 +0100

    log_dir

--- lib_sh_utils/src/background.sh
+++ lib_sh_utils/src/background.sh
@@ -2,9 +2,9 @@
 
 run_command_in_background() {
 	local command_to_run="$1"
-	local logdir="${logdir:-${2:-./}}"
+	local log_dir="${log_dir:-${2:-./}}"
 	local file_name
 	file_name="$(date --iso)-$(hostname).txt"
-	mkdir -p "$logdir"
-	eval "nohup $command_to_run &>> \"$logdir/$file_name\" &"
+	mkdir -p "$log_dir"
+	eval "nohup $command_to_run &>> \"$log_dir/$file_name\" &"
 }

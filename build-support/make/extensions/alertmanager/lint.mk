9a5adea31e27b5bb54f0fcd06456d63ed690d589
Cristian Matache <cristianmatache@hotmail.com>
2021-09-24 14:22:27 +0100

    Update lint.mk

--- build-support/make/extensions/alertmanager/lint.mk
+++ build-support/make/extensions/alertmanager/lint.mk
@@ -1,6 +1,6 @@
 # find .. --exec always has exit code 0 -> use find | xargs
 amtool-check-config:
 	$(eval on := $(onam))
-	$(eval amhome := $(shell source lib_sh_utils/src/commands.sh && find_command_home alertmanager ~/alertmanager/ ALERTMANAGER_HOME))
+	$(eval amhome := $(shell source lib_sh_utils/src/commands.sh && find_command_home alertmanager ~/apps/alertmanager/ ALERTMANAGER_HOME))
 	if $(call lang,$(on),".*alertmanager/.*\.yml"); then \
 	find $(call solve_aliases,$(on)) -type f -regex ".*alertmanager.*\.yml" | xargs --no-run-if-empty "$(amhome)/amtool" check-config; fi

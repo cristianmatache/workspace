23e0e6a8eb431203053dd974df5470b766eb4212
Cristian Matache <cristianmatache@hotmail.com>
2021-09-28 23:13:04 +0100

    git-bash-integration and mac compatibility

--- build-support/make/config/python.mk
+++ build-support/make/config/python.mk
@@ -5,7 +5,7 @@ line_len=120
 
 # Config files
 MYPY_CONFIG=build-support/python/tools-config/mypy.ini
-PYLINT_CONFIG=build-support/python/tools-config/.pylintrc
+PYLINT_CONFIG=build-support/python/tools-config/pylintrc
 FLAKE8_CONFIG=build-support/python/tools-config/.flake8
 ISORT_CONFIG=build-support/python/tools-config/pyproject.toml
 BLACK_CONFIG=build-support/python/tools-config/pyproject.toml

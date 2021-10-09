a9bf85157f337930b8b2445427fbcd69962ea0bb
Cristian Matache <cristianmatache@hotmail.com>
2021-10-09 21:05:00 +0100

    better config

--- build-support/make/config/markdown.mk
+++ build-support/make/config/markdown.mk
@@ -4,7 +4,7 @@ DEFAULT_MD_ENV=3rdparty/md-env-ws
 DEFAULT_NPM_DEV_MD_DEPS=$(DEFAULT_MD_ENV)/npm-dev-deps.txt
 
 # Binaries
-MARKDOWNLINT_BIN="./3rdparty/md-env-ws/node_modules/.bin/markdownlint"
+MARKDOWNLINT_BIN="./$(DEFAULT_MD_ENV)/node_modules/.bin/markdownlint"
 
 # Flags
 MARKDOWNLINT_FLAGS=--config build-support/markdown/tools-config/markdownlint.yml
--- build-support/make/config/multi.mk
+++ build-support/make/config/multi.mk
@@ -2,3 +2,5 @@ DEFAULT_PRETTIER_ENV=3rdparty/prettier-env-ws
 
 # Binaries
 PRETTIER_BIN="./$(DEFAULT_PRETTIER_ENV)/node_modules/.bin/prettier"
+
+PRETTIER_FLAGS=--end-of-line auto
